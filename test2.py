92bbe8cbfe96df763e7da07d01b2a4ef96400340
riezario <100576052+riezario@users.noreply.github.com>
2023-02-12 10:09:32 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,10 +2,10 @@
 import streamlit as st
 
 # meminta pengguna untuk memasukkan jumlah produk yang akan diproduksi
-jumlah_produk = st.number_input("Masukkan jumlah produk yang akan diproduksi: ")
+jumlah_produk = int(st.number_input("Masukkan jumlah produk yang akan diproduksi: "))
 
 # meminta pengguna untuk memasukkan jumlah bahan baku per produk
-jumlah_bahan_baku = st.number_input("Berapa banyak bahan baku yang digunakan per produk? ")
+jumlah_bahan_baku = int(st.number_input("Berapa banyak bahan baku yang digunakan per produk? "))
 
 # membuat list untuk menyimpan informasi bahan baku
 bahan_baku = []
@@ -13,8 +13,8 @@ bahan_baku = []
 # meminta pengguna untuk memasukkan informasi bahan baku
 for i in range(jumlah_bahan_baku):
     nama_bahan_baku = st.text_input(f"Masukkan nama bahan baku {i+1}: ")
-    jumlah_bahan_baku_per_produk = st.number_input(f"Masukkan jumlah bahan baku {i+1} per produk: ")
-    harga_bahan_baku = st.number_input(f"Masukkan harga bahan baku {i+1} per satuan: ")
+    jumlah_bahan_baku_per_produk = float(st.number_input(f"Masukkan jumlah bahan baku per produk: "))
+    harga_bahan_baku = float(st.number_input(f"Masukkan harga bahan baku per satuan: "))
     bahan_baku.append((nama_bahan_baku, jumlah_bahan_baku_per_produk, harga_bahan_baku))
 
 # menghitung kebutuhan bahan baku dan biaya total
