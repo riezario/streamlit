67ccd693b9497b0454e47ed1f91be5661b3c259a
riezario <100576052+riezario@users.noreply.github.com>
2023-02-12 10:40:02 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,31 +1,40 @@
+# memasukkan pustaka streamlit
 import streamlit as st
 
-# Daftar produk yang tersedia
-produk_tersedia = {
-    "Produk A": {"Bahan Baku 1": 10, "Bahan Baku 2": 5, "Bahan Baku 3": 2, "Harga": 1000},
-    "Produk B": {"Bahan Baku 1": 5, "Bahan Baku 2": 8, "Bahan Baku 3": 1, "Harga": 1500},
-    "Produk C": {"Bahan Baku 1": 3, "Bahan Baku 2": 6, "Bahan Baku 3": 4, "Harga": 2000},
-}
+# membuat list untuk menyimpan informasi produk
+produk = ["Produk A", "Produk B", "Produk C", "Produk D"]
 
-# Meminta pengguna untuk memasukkan nama produk
-nama_produk = st.selectbox("Pilih produk yang akan diproduksi:", list(produk_tersedia.keys()))
+# membuat list untuk menyimpan informasi bahan baku
+bahan_baku = ["Bahan A", "Bahan B", "Bahan C", "Bahan D"]
 
-# Meminta pengguna untuk memasukkan jumlah produk yang akan diproduksi
+# meminta pengguna untuk memilih produk yang akan diproduksi
+pilihan_produk = st.selectbox("Pilih produk yang akan diproduksi:", produk)
+
+# meminta pengguna untuk memasukkan jumlah produk yang akan diproduksi
 jumlah_produk = int(st.number_input("Masukkan jumlah produk yang akan diproduksi: "))
 
-# Mengambil informasi produk yang dipilih oleh pengguna
-informasi_produk = produk_tersedia[nama_produk]
+# meminta pengguna untuk memilih bahan baku yang akan digunakan
+pilihan_bahan_baku = st.multiselect("Pilih bahan baku yang akan digunakan:", bahan_baku)
 
-# Menghitung kebutuhan bahan baku dan biaya total
+# membuat list untuk menyimpan informasi bahan baku yang akan digunakan
+bahan_baku_digunakan = []
+
+# meminta pengguna untuk memasukkan informasi bahan baku
+for bahan in pilihan_bahan_baku:
+    jumlah_bahan_baku_per_produk = float(st.number_input(f"Berapa banyak bahan {bahan} yang digunakan per produk? "))
+    harga_bahan_baku = float(st.number_input(f"Berapa harga bahan {bahan} per satuan? "))
+    bahan_baku_digunakan.append((bahan, jumlah_bahan_baku_per_produk, harga_bahan_baku))
+
+# menghitung kebutuhan bahan baku dan biaya total
 total_biaya = 0
-st.write("Kebutuhan Bahan Baku:")
-for bahan_baku, jumlah_bahan_baku in informasi_produk.items():
-    if bahan_baku == "Harga":
-        continue
-    kebutuhan_bahan_baku = jumlah_bahan_baku * jumlah_produk
-    biaya = kebutuhan_bahan_baku * informasi_produk["Harga"]
+for bahan in bahan_baku_digunakan:
+    nama_bahan_baku, jumlah_bahan_baku_per_produk, harga_bahan_baku = bahan
+    kebutuhan_bahan_baku = jumlah_produk * jumlah_bahan_baku_per_produk
+    biaya = kebutuhan_bahan_baku * harga_bahan_baku
     total_biaya += biaya
-    st.write(f"- {bahan_baku}: {kebutuhan_bahan_baku}")
 
-# Menampilkan hasil
-st.write(f"Total Biaya: {total_biaya}")
+# menampilkan hasil
+st.write("Informasi Produk:")
+st.write(f"- Nama Produk: {pilihan_produk}")
+st.write(f"- Jumlah Produk: {jumlah_produk}")
+st.write("Kebutuhan Bahan Baku:")
