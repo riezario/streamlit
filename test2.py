435bfbf6193ffac5ef7f73be5b545b0402a09d99
riezario <100576052+riezario@users.noreply.github.com>
2023-02-12 09:42:46 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,9 +1,37 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Aplikasi Sederhana Streamlit")
+def calculate_raw_material_requirement(marketing_plan, raw_materials):
+    raw_material_requirement = {}
+    for product, quantity in marketing_plan.items():
+        for raw_material, usage in raw_materials[product].items():
+            if raw_material not in raw_material_requirement:
+                raw_material_requirement[raw_material] = 0
+            raw_material_requirement[raw_material] += usage * quantity
+    return raw_material_requirement
 
-nama = st.text_input("Masukkan nama Anda:", "")
+def main():
+    st.title("Raw Material Calculator")
+    
+    marketing_plan = {
+        "Product A": 100,
+        "Product B": 200,
+        "Product C": 300
+    }
+    
+    raw_materials = {
+        "Product A": {"Raw Material X": 1, "Raw Material Y": 2},
+        "Product B": {"Raw Material X": 2, "Raw Material Y": 1, "Raw Material Z": 3},
+        "Product C": {"Raw Material Y": 1, "Raw Material Z": 2}
+    }
+    
+    raw_material_requirement = calculate_raw_material_requirement(marketing_plan, raw_materials)
+    
+    st.write("Marketing Plan:")
+    st.write(pd.DataFrame.from_dict(marketing_plan, orient="index", columns=["Quantity"]))
+    
+    st.write("Raw Material Requirements:")
+    st.write(pd.DataFrame.from_dict(raw_material_requirement, orient="index", columns=["Quantity"]))
 
-if st.button("Submit"):
-    result = f"Hello, {nama}!"
-    st.success(result)
+if __name__ == "__main__":
+    main()
