c3e4c5dbe8927cceead4e7534406ab0402fc611b
riezario <100576052+riezario@users.noreply.github.com>
2023-02-14 11:43:20 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,14 +4,17 @@ import numpy as np
 
 st.title("Kebutuhan Bahan Baku")
 
+# Inisialisasi session state
+if "products" not in st.session_state:
+    st.session_state.products = pd.DataFrame({"Nama Produk": [], "Jumlah Produksi": []})
+
 with st.sidebar:
     st.header("Marketing Plan")
     product_name = st.text_input("Nama Produk")
     production_qty = st.number_input("Jumlah Produksi", min_value=0, step=1)
-
-data = {"Nama Produk": ["Produk A", "Produk B", "Produk C"],
-        "Jumlah Produksi": [100, 200, 300]}
-df = pd.DataFrame(data)
+    if st.button("Tambahkan"):
+        new_product = pd.DataFrame({"Nama Produk": [product_name], "Jumlah Produksi": [production_qty]})
+        st.session_state.products = pd.concat([st.session_state.products, new_product], ignore_index=True)
 
 st.write("Daftar Produk")
-st.write(df)
+st.write(st.session_state.products)
