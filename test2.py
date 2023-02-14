58094c14e20fdf81186e393965422f4101a16765
riezario <100576052+riezario@users.noreply.github.com>
2023-02-14 12:53:25 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 st.title("Kebutuhan Bahan Baku")
 
 # Inisialisasi session state
 if "products" not in st.session_state:
-    st.session_state.products = pd.DataFrame({"Nama Produk": [], "Jumlah Produksi": []})
+    st.session_state.products = pd.DataFrame({"Customer": [], "Produk": [], "Count": [], "Jenis": [], "Quantity": [], "Harga": []})
 
 with st.sidebar:
     st.header("Marketing Plan")
+    customer_name = st.text_input("Nama Customer")
     product_name = st.text_input("Nama Produk")
-    production_qty = st.number_input("Jumlah Produksi", min_value=0, step=1)
+    count = st.number_input("Count", min_value=0, step=1)
+    jenis = st.selectbox("Jenis", options=["A", "B", "C"])
+    quantity = st.number_input("Quantity", min_value=0, step=1)
+    harga = st.number_input("Harga", min_value=0.0, step=0.01)
     if st.button("Tambahkan"):
-        new_product = pd.DataFrame({"Nama Produk": [product_name], "Jumlah Produksi": [production_qty]})
+        new_product = pd.DataFrame({"Customer": [customer_name], "Produk": [product_name], "Count": [count], "Jenis": [jenis], "Quantity": [quantity], "Harga": [harga]})
         st.session_state.products = pd.concat([st.session_state.products, new_product], ignore_index=True)
 
 st.write("Daftar Produk")
